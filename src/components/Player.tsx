708e51c6862536e9c0f5aa3bd9707c8b938299f6
james <james.barbosa@gmail.com>
2024-02-08 07:31:55 +0800

    Initialized styling Added player prototype

---
+++ src/components/Player.tsx
@@ -0,0 +1,11 @@
+export function Player() {
+    return (
+        <section id="player">
+            <h2>Welcome null</h2>
+            <p>
+                <input type="text" />
+                <button>Set Name</button>
+            </p>
+        </section>
+    );
+}
