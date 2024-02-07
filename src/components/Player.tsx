c730f2e75d8c22cbf49ef7ad58b97303dbaf23fd
james <james.barbosa@gmail.com>
2024-02-08 07:52:31 +0800

    Added feature to update name

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,10 +1,20 @@
+import { useRef, useState } from "react";
+
 export function Player() {
+
+    const [playerName, setPlayerName] = useState(null);
+    const name = useRef();
+
+    function handleChangeName() {
+        setPlayerName(name.current.value)
+    }
+
     return (
         <section id="player">
-            <h2>Welcome null</h2>
+            <h2>Welcome {playerName ?? 'unknown'}</h2>
             <p>
-                <input type="text" />
-                <button>Set Name</button>
+                <input ref={name} type="text" />
+                <button onClick={handleChangeName}>Set Name</button>
             </p>
         </section>
     );
