60cf470441a5735eda8d60cf302fb010d5fea55e
james <james.barbosa@gmail.com>
2024-02-08 13:06:42 +0800

    Create portal to move ResultModal to modal div

--- src/components/ResultModal.tsx
+++ src/components/ResultModal.tsx
@@ -1,32 +1,34 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
+import { createPortal } from "react-dom";
 
-const fRef = forwardRef(function ResultModal({result, targetTime, timeLeft, onClose}, ref) {
-    const dialog = useRef(); 
+const fRef = forwardRef(function ResultModal({ result, targetTime, timeLeft, onClose }, ref) {
+    const dialog = useRef();
     const isUserLost = timeLeft <= 0;
 
     const score = Math.round((1 - timeLeft / targetTime) * 100);
 
-    useImperativeHandle(ref, ()=> {
+    useImperativeHandle(ref, () => {
         return {
             open() {
                 dialog.current.showModal();
             }
         }
-     })
+    })
 
 
-    return <dialog ref={dialog} onClose={onClose} className="result-modal">
-        <h2>{isUserLost ? 'You Lost' : `Your score is ${score}`}</h2>
-        <p>
-            Your target time was {targetTime} seconds
-        </p>
-        <p>
-            You stopped the timer at {timeLeft} seconds
-        </p>
-        <form method="dialog">
-            <button onClick={onClose}>Close</button>
-        </form>
-    </dialog>
+    return createPortal(
+        <dialog ref={dialog} onClose={onClose} className="result-modal">
+            <h2>{isUserLost ? 'You Lost' : `Your score is ${score}`}</h2>
+            <p>
+                Your target time was {targetTime} seconds
+            </p>
+            <p>
+                You stopped the timer at {timeLeft} seconds
+            </p>
+            <form method="dialog">
+                <button onClick={onClose}>Close</button>
+            </form>
+        </dialog>, document.getElementById("modal"))
 })
 
 export default fRef;
