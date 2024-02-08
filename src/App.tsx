59977f0cc8eb70b6ae7b573cc422308b9e6a6bcf
james <james.barbosa@gmail.com>
2024-02-08 08:08:23 +0800

    Added Timer Challenge Updated styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Player } from './components/Player'
+import TimerChallenge from './components/TimerChallenge'
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1}/>
+        <TimerChallenge title="Medium" targetTime={5}/>
+        <TimerChallenge title="Hard" targetTime={10}/>
+        <TimerChallenge title="Very Hard" targetTime={15}/>
+      </div>
     </>
   );
 }
